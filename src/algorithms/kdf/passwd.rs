b84163c7d6ea0aeb1ec86fff2a2db4b5d2a5ca32
韶光 <admin@gloryouth.com>
2025-07-17 20:59:46 +0800

    Refactor KDF password algorithms to use parameter structs
    This commit modifies the KdfPasswordAlgorithm enum to utilize an Argon2Params struct for better parameter management. It introduces new builder methods for creating Argon2 and PBKDF2 algorithms with default parameters, enhancing flexibility and code clarity. Additionally, the Argon2Wrapper is updated to handle default and parameterized configurations, improving the overall structure and usability of the KDF implementations.

--- src/algorithms/kdf/passwd.rs
+++ src/algorithms/kdf/passwd.rs
@@ -1,20 +1,26 @@
 use crate::algorithms::HashAlgorithmEnum;
 use bincode::{Decode, Encode};
 
+
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, Hash, Decode, Encode, serde::Serialize, serde::Deserialize,
+)]
+pub struct Argon2Params {
+    pub m_cost: u32,
+    pub t_cost: u32,
+    pub p_cost: u32,
+}
+
 ///
 /// 基于密码的 KDF 算法枚举。
 #[derive(
     Debug, Clone, Copy, PartialEq, Eq, Hash, Decode, Encode, serde::Serialize, serde::Deserialize,
 )]
 pub enum KdfPasswordAlgorithm {
-    Argon2 {
-        m_cost: u32,
-        t_cost: u32,
-        p_cost: u32,
-    },
+    Argon2(Option<Argon2Params>),
     Pbkdf2 {
         hash: HashAlgorithmEnum,
-        c: u32,
+        c: Option<u32>,
     },
 }
 
@@ -27,17 +33,58 @@ impl KdfPasswordAlgorithm {
 pub struct KdfPasswordAlgorithmBuilder;
 
 impl KdfPasswordAlgorithmBuilder {
-    pub fn argon2(self, m_cost: u32, t_cost: u32, p_cost: u32) -> KdfPasswordAlgorithm {
-        KdfPasswordAlgorithm::Argon2 { m_cost, t_cost, p_cost }
+    pub fn argon2_default(self) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Argon2(None)
     }
-    pub fn pbkdf2_sha256(self, c: u32) -> KdfPasswordAlgorithm {
-        KdfPasswordAlgorithm::Pbkdf2 { hash: HashAlgorithmEnum::Sha256, c }
+
+    pub fn pbkdf2_sha256_default(self) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Pbkdf2 {
+            hash: HashAlgorithmEnum::Sha256,
+            c: None,
+        }
     }
-    pub fn pbkdf2_sha384(self, c: u32) -> KdfPasswordAlgorithm {
-        KdfPasswordAlgorithm::Pbkdf2 { hash: HashAlgorithmEnum::Sha384, c }
+
+    pub fn pbkdf2_sha384_default(self) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Pbkdf2 {
+            hash: HashAlgorithmEnum::Sha384,
+            c: None,
+        }
     }
-    pub fn pbkdf2_sha512(self, c: u32) -> KdfPasswordAlgorithm {
-        KdfPasswordAlgorithm::Pbkdf2 { hash: HashAlgorithmEnum::Sha512, c }
+
+    pub fn pbkdf2_sha512_default(self) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Pbkdf2 {
+            hash: HashAlgorithmEnum::Sha512,
+            c: None,
+        }
+    }
+
+    pub fn argon2_with_params(self, m_cost: u32, t_cost: u32, p_cost: u32) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Argon2(Some(Argon2Params {
+            m_cost,
+            t_cost,
+            p_cost,
+        }))
+    }
+
+    pub fn pbkdf2_sha256_with_params(self, c: u32) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Pbkdf2 {
+            hash: HashAlgorithmEnum::Sha256,
+            c: Some(c),
+        }
+    }
+
+    pub fn pbkdf2_sha384_with_params(self, c: u32) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Pbkdf2 {
+            hash: HashAlgorithmEnum::Sha384,
+            c: Some(c),
+        }
+    }
+
+    pub fn pbkdf2_sha512_with_params(self, c: u32) -> KdfPasswordAlgorithm {
+        KdfPasswordAlgorithm::Pbkdf2 {
+            hash: HashAlgorithmEnum::Sha512,
+            c: Some(c),
+        }
     }
 }
 
@@ -49,19 +96,31 @@ impl KdfPasswordAlgorithm {
             Argon2Wrapper, Pbkdf2Sha256Wrapper, Pbkdf2Sha384Wrapper, Pbkdf2Sha512Wrapper,
         };
         match self {
-            KdfPasswordAlgorithm::Argon2 { m_cost, t_cost, p_cost } => {
-                KdfPasswordWrapper::new(Box::new(Argon2Wrapper::new(m_cost, t_cost, p_cost)))
+            KdfPasswordAlgorithm::Argon2(Some(params)) => {
+                KdfPasswordWrapper::new(Box::new(Argon2Wrapper::new(params.m_cost, params.t_cost, params.p_cost)))
+            }
+            KdfPasswordAlgorithm::Argon2(None) => {
+                KdfPasswordWrapper::new(Box::new(Argon2Wrapper::default()))
             }
             KdfPasswordAlgorithm::Pbkdf2 { hash, c } => {
-                match hash {
-                    HashAlgorithmEnum::Sha256 => {
+                match (hash, c) {
+                    (HashAlgorithmEnum::Sha256, Some(c)) => {
                         KdfPasswordWrapper::new(Box::new(Pbkdf2Sha256Wrapper::new(c)))
                     }
-                    HashAlgorithmEnum::Sha384 => {
+                    (HashAlgorithmEnum::Sha384, Some(c)) => {
                         KdfPasswordWrapper::new(Box::new(Pbkdf2Sha384Wrapper::new(c)))
                     }
-                    HashAlgorithmEnum::Sha512 => {
+                    (HashAlgorithmEnum::Sha512, Some(c)) => {
                         KdfPasswordWrapper::new(Box::new(Pbkdf2Sha512Wrapper::new(c)))
+                    }
+                    (HashAlgorithmEnum::Sha256, None) => {
+                        KdfPasswordWrapper::new(Box::new(Pbkdf2Sha256Wrapper::default()))
+                    }
+                    (HashAlgorithmEnum::Sha384, None) => {
+                        KdfPasswordWrapper::new(Box::new(Pbkdf2Sha384Wrapper::default()))
+                    }
+                    (HashAlgorithmEnum::Sha512, None) => {
+                        KdfPasswordWrapper::new(Box::new(Pbkdf2Sha512Wrapper::default()))
                     }
                 }
             }
--- src/wrappers/kdf/passwd.rs
+++ src/wrappers/kdf/passwd.rs
@@ -1,5 +1,5 @@
 use crate::algorithms::HashAlgorithmEnum;
-use crate::algorithms::kdf::passwd::KdfPasswordAlgorithm;
+use crate::algorithms::kdf::passwd::{KdfPasswordAlgorithm, Argon2Params};
 use crate::error::{Error, Result};
 use crate::traits::KdfPasswordAlgorithmTrait;
 use seal_crypto::prelude::PasswordBasedDerivation;
@@ -9,107 +9,133 @@ use seal_crypto::schemes::kdf::{
 };
 use seal_crypto::secrecy::SecretBox;
 use seal_crypto::zeroize::Zeroizing;
-use crate::define_wrapper;
 use std::ops::Deref;
 
-macro_rules! impl_kdf_argon2_algorithm {
-    ($wrapper:ident, $algo:ty, $algo_enum:path) => {
-        define_wrapper!(
-            @struct_with_algorithm,
-            $wrapper,
-            $algo,
-            KdfPasswordAlgorithmTrait,
-            {
-                fn new(m_cost: u32, t_cost: u32, p_cost: u32) -> Self {
-                    Self {
-                        algorithm: <$algo>::new(m_cost, t_cost, p_cost),
-                    }
-                }
-            },
-            {
-                fn derive(
-                    &self,
-                    password: &SecretBox<[u8]>,
-                    salt: &[u8],
-                    output_len: usize,
-                ) -> Result<Zeroizing<Vec<u8>>> {
-                    self.algorithm
-                        .derive(password, salt, output_len)
-                        .map(|dk| dk.0)
-                        .map_err(Error::from)
-                }
 
-                fn algorithm(&self) -> KdfPasswordAlgorithm {
-                    $algo_enum {
-                        m_cost: self.algorithm.m_cost,
-                        t_cost: self.algorithm.t_cost,
-                        p_cost: self.algorithm.p_cost,
-                    }
-                }
+#[derive(Clone)]
+pub struct Argon2Wrapper {
+    algorithm: Argon2,
+    is_default: bool,
+}
 
-                fn clone_box(&self) -> Box<dyn KdfPasswordAlgorithmTrait> {
-                    Box::new(self.clone())
-                }
-            }
-        );
-    };
+impl Argon2Wrapper {
+    pub fn new(m_cost: u32, t_cost: u32, p_cost: u32) -> Self {
+        Self {
+            algorithm: Argon2::new(m_cost, t_cost, p_cost),
+            is_default: false,
+        }
+    }
+}
+
+impl Default for Argon2Wrapper {
+    fn default() -> Self {
+        Self {
+            algorithm: Argon2::default(),
+            is_default: true,
+        }
+    }
+}
+
+impl KdfPasswordAlgorithmTrait for Argon2Wrapper {
+    fn derive(
+        &self,
+        password: &SecretBox<[u8]>,
+        salt: &[u8],
+        output_len: usize,
+    ) -> Result<Zeroizing<Vec<u8>>> {
+        self.algorithm
+            .derive(password, salt, output_len)
+            .map(|dk| dk.0)
+            .map_err(Error::from)
+    }
+
+    fn algorithm(&self) -> KdfPasswordAlgorithm {
+        if self.is_default {
+            KdfPasswordAlgorithm::Argon2(None)
+        } else {
+            KdfPasswordAlgorithm::Argon2(Some(Argon2Params {
+                m_cost: self.algorithm.m_cost,
+                t_cost: self.algorithm.t_cost,
+                p_cost: self.algorithm.p_cost,
+            }))
+        }
+    }
+
+    fn clone_box(&self) -> Box<dyn KdfPasswordAlgorithmTrait> {
+        Box::new(self.clone())
+    }
 }
 
 macro_rules! impl_kdf_pbkdf_algorithm {
-    ($wrapper:ident, $algo:ty, $hash_enum:expr) => {
-        define_wrapper!(
-            @struct_with_algorithm,
-            $wrapper,
-            $algo,
-            KdfPasswordAlgorithmTrait,
-            {
-                fn new(#[allow(unused_variables)] c: u32) -> Self {
-                    Self {
-                        algorithm: <$algo>::new(c),
-                    }
-                }
-            },
-            {
-                fn derive(
-                    &self,
-                    password: &SecretBox<[u8]>,
-                    salt: &[u8],
-                    output_len: usize,
-                ) -> Result<Zeroizing<Vec<u8>>> {
-                    self.algorithm
-                        .derive(password, salt, output_len)
-                        .map(|dk| dk.0)
-                        .map_err(Error::from)
-                }
+    ($wrapper:ident, $algo:ty, $hash_enum:expr, $kind:path) => {
+        #[derive(Clone)]
+        pub struct $wrapper {
+            algorithm: $algo,
+            is_default: bool,
+        }
 
-                fn algorithm(&self) -> KdfPasswordAlgorithm {
-                    unimplemented!()
-                }
-
-                fn clone_box(&self) -> Box<dyn KdfPasswordAlgorithmTrait> {
-                    Box::new(self.clone())
+        impl $wrapper {
+            pub fn new(c: u32) -> Self {
+                Self {
+                    algorithm: <$algo>::new(c),
+                    is_default: false,
                 }
             }
-        );
+        }
+        
+        impl Default for $wrapper {
+            fn default() -> Self {
+                Self {
+                    algorithm: <$algo>::default(),
+                    is_default: true,
+                }
+            }
+        }
+
+        impl KdfPasswordAlgorithmTrait for $wrapper {
+            fn derive(
+                &self,
+                password: &SecretBox<[u8]>,
+                salt: &[u8],
+                output_len: usize,
+            ) -> Result<Zeroizing<Vec<u8>>> {
+                self.algorithm
+                    .derive(password, salt, output_len)
+                    .map(|dk| dk.0)
+                    .map_err(Error::from)
+            }
+
+            fn algorithm(&self) -> KdfPasswordAlgorithm {
+                KdfPasswordAlgorithm::Pbkdf2 {
+                    hash: $hash_enum,
+                    c: if self.is_default { None } else { Some(self.algorithm.iterations) },
+                }
+            }
+
+            fn clone_box(&self) -> Box<dyn KdfPasswordAlgorithmTrait> {
+                Box::new(self.clone())
+            }
+        }
     };
 }
-
-impl_kdf_argon2_algorithm!(Argon2Wrapper, Argon2, KdfPasswordAlgorithm::Argon2);
 
 impl_kdf_pbkdf_algorithm!(
     Pbkdf2Sha256Wrapper,
     Pbkdf2Sha256,
-    HashAlgorithmEnum::Sha256
+    HashAlgorithmEnum::Sha256,
+    KdfPasswordAlgorithmKind::Pbkdf2Sha256
 );
 impl_kdf_pbkdf_algorithm!(
     Pbkdf2Sha384Wrapper,
     Pbkdf2Sha384,
-    HashAlgorithmEnum::Sha384
+    HashAlgorithmEnum::Sha384,
+    KdfPasswordAlgorithmKind::Pbkdf2Sha384
 );
 impl_kdf_pbkdf_algorithm!(
     Pbkdf2Sha512Wrapper,
     Pbkdf2Sha512,
-    HashAlgorithmEnum::Sha512
+    HashAlgorithmEnum::Sha512,
+    KdfPasswordAlgorithmKind::Pbkdf2Sha512
 );
 
 #[derive(Clone)]
